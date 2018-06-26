d6c4b12c260f80598dbe020fcf69675f447cccaa
Javier De Las Casas <javier.delascasaschung@gmail.com>
2018-06-26 14:04:09 +0200

    Add constructor for GameObjekt/Player class

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,5 +1,17 @@
+import com.googlecode.lanterna.terminal.Terminal;
+
 public abstract class GameObject {
-    int x, y;
 
+    protected int x, y;
+    protected String symbol;
+    protected Terminal.Color color;
+    protected int speed;
 
+    public GameObject(int x, int y, String symbol, Terminal.Color color, int speed) {
+        this.x = x;
+        this.y = y;
+        this.symbol = symbol;
+        this.color = color;
+        this.speed = speed;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,9 @@
+
+import static com.googlecode.lanterna.terminal.Terminal.*;
+
 public class Player extends GameObject {
+
+    public Player(int x, int y) {
+        super(x,y,  "P", Color.WHITE, 3);
+    }
 }
